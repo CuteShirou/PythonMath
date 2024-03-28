1b81d2bce5bb26ee118e20752d60c00185daf9eb
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-03-28 12:47:38 +0100

    Update ProduitScalaire.py

--- Tools/ProduitScalaire.py
+++ Tools/ProduitScalaire.py
@@ -1,12 +1,10 @@
-import numpy as np
-
 def calculer_produit_scalaire(vecteur1, vecteur2):
-    produit_scalaire = np.dot(vecteur1, vecteur2)
+    produit_scalaire = sum(v1 * v2 for v1, v2 in zip(vecteur1, vecteur2))
     return produit_scalaire
 
 # Exemple d'utilisation
-vecteur1 = np.array([1, 2, 3])
-vecteur2 = np.array([4, 5, 6])
+vecteur1 = [1, 2, 3]
+vecteur2 = [4, 5, 6]
 
 resultat = calculer_produit_scalaire(vecteur1, vecteur2)
 print("Le produit scalaire des vecteurs est :", resultat)
