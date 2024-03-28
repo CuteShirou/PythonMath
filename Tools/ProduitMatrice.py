695453d4f6f0ef51619ed300ce17061e1bc57053
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-03-28 11:32:44 +0100

    Create ProduitMatrice.py

---
+++ Tools/ProduitMatrice.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Définition des matrices
+matrice1 = np.array([[1, 2, 3], [4, 5, 6]])
+matrice2 = np.array([[7, 8], [9, 10], [11, 12]])
+
+# Calcul du produit de matrices
+produit = np.dot(matrice1, matrice2)
+
+# Affichage du résultat
+print("Produit de matrices :")
+print(produit)
